b5c4c903dc888918585baf99104f83955ef88950
Julien Blégean <julien@kompose.co>
2023-05-25 17:34:24 +0200

    initial commit

---
+++ src/rooms/MyRoom.ts
@@ -0,0 +1,94 @@
+import { Room, Client } from "@colyseus/core";
+import { MyRoomState, Player } from "./schema/MyRoomState";
+
+export class MyRoom extends Room<MyRoomState> {
+
+  onCreate(options: any) {
+    this.setState(new MyRoomState());
+
+    this.setSimulationInterval((deltaTime) => {
+      this.update(deltaTime);
+  });
+
+    // handle player input
+    this.onMessage(0, (client, data) => {
+      // get reference to the player who sent the message
+      const player = this.state.players.get(client.sessionId);
+
+      player.inputQueue.push(data);
+    });
+
+    this.onMessage("trap-create", (client, message) => {
+      // broadcast a message to all clients
+      this.broadcast("trap-create", message);
+  });
+
+
+  this.onMessage("trap-move", (client, message) => {
+    // broadcast a message to all clients
+    this.broadcast("trap-move", message);
+  });
+
+  this.onMessage("trap-settle", (client, message) => {
+    // broadcast a message to all clients
+    this.broadcast("trap-settle", message);
+  });
+  }
+
+  onJoin(client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+
+    const mapWidth = 800;
+    const mapHeight = 600;
+
+    // create Player instance
+    const player = new Player();
+
+    // place Player at a random position
+    player.x = 100
+    player.y = 450
+
+    // place player in the map of players by its sessionId
+    // (client.sessionId is unique per connection!)
+    this.state.players.set(client.sessionId, player);
+}
+
+onLeave(client: Client, consented: boolean) {
+  console.log(client.sessionId, "left!");
+
+  this.state.players.delete(client.sessionId);
+}
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+  update(deltaTime: number) {
+    const velocity = 2;
+
+    this.state.players.forEach(player => {
+        let input: any;
+
+        // dequeue player inputs
+        while (input = player.inputQueue.shift()) {
+
+        
+          if (input.x) {
+            player.x = input.x;
+        
+          } 
+        
+          if (input.y) {
+            player.y = input.y;
+        
+          } 
+
+          if(input.animation) {
+            player.animation = input.animation
+          }
+
+        }
+    });
+}
+
+}
---
+++ src/rooms/schema/MyRoomState.ts
@@ -0,0 +1,17 @@
+import { MapSchema, Schema, type } from "@colyseus/schema";
+
+export class Player extends Schema {
+    @type("number") x: number;
+    @type("number") y: number;
+
+    trapPositions: { x: number, y: number }[]
+    trapNames: string[]
+
+    @type("string") animation: string;
+
+    inputQueue: any[] = []
+}
+
+export class MyRoomState extends Schema {
+  @type({ map: Player }) players = new MapSchema<Player>();
+}
